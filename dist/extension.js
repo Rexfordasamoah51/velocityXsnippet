3d81a898303c190a18bd2e868983d23601e6a27f
Rexfordasamoah51 <rexfordasamoah51@gmail.com>
2021-02-07 17:35:09 +0000

    convert svg to png🥗

--- dist/extension.js
+++ dist/extension.js
@@ -29935,7 +29935,7 @@ const commands_1 = __webpack_require__(/*! ./commands */ "./src/commands/index.t
 const code_actions_1 = __webpack_require__(/*! ./code-actions */ "./src/code-actions/index.ts");
 const DART_MODE = { language: "dart", scheme: "file" };
 function activate(_context) {
-    if (vscode_1.workspace.getConfiguration("bloc").get("checkForUpdates")) {
+    if (vscode_1.workspace.getConfiguration("velocity_x").get("checkForUpdates")) {
         utils_1.analyzeDependencies();
     }
     _context.subscriptions.push(vscode_1.commands.registerCommand("extension.wrap-vstack", commands_1.wrapWithVStack), vscode_1.commands.registerCommand("extension.wrap-vstackscroll", commands_1.wrapWithVStackScroll), vscode_1.commands.registerCommand("extension.wrap-hstack", commands_1.wrapWithHStack), vscode_1.commands.registerCommand("extension.wrap-hstackscroll", commands_1.wrapWithHstackScroll), vscode_1.commands.registerCommand("extension.wrap-vxdisclist", commands_1.wrapWithVxDiscList), vscode_1.commands.registerCommand("extension.wrap-vscrollvertical", commands_1.wrapWithVxScrollVerticial), vscode_1.languages.registerCodeActionsProvider(DART_MODE, new code_actions_1.VelocityXActionProvider()));
